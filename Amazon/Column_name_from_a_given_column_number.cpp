b7703720e18f998294dace8492e83ac85e56d60a
Tanmoyee Roy <61410535+tanmoyee04@users.noreply.github.com>
2022-01-07 12:17:04 +0530

    Create Column_name_from_a_given_column_number.cpp

---
+++ Amazon/Column_name_from_a_given_column_number.cpp
@@ -0,0 +1,38 @@
+/* Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+Excel columns has a pattern like A, B, C, … ,Z, AA, AB, AC,…. ,AZ, BA, BB, … ZZ, AAA, AAB ….. etc. In other words, column 1 is named as “A”, column 2 as “B”, column 27 as “AA” 
+and so on. */
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string ans="";
+        while(n>0){
+            char ch= 'A'+(n-1)%26;
+            ans=ch+ans;
+            n=(n-1)/26;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends
